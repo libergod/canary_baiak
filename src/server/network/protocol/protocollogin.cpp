c84192c3e868ecc0de0616b46b08858d7c54b053
LIBERGOD <37589902+libergod@users.noreply.github.com>
2023-07-27 13:23:25 -0300

    Revert "x"
    This reverts commit 0035843976b2b076684a1a355d825f5e3e215715.

--- src/server/network/protocol/protocollogin.cpp
+++ src/server/network/protocol/protocollogin.cpp
@@ -30,6 +30,7 @@ void ProtocolLogin::disconnectClient(const std::string &message) {
 
 void ProtocolLogin::getCharacterList(const std::string &accountIdentifier, const std::string &password) {
 	account::Account account;
+
 	account.setProtocolCompat(oldProtocol);
 
 	if (oldProtocol && !g_configManager().getBoolean(OLD_PROTOCOL)) {
