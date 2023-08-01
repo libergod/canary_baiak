7245e4c035b2b1bcbdd09971d539c98fddc15c3a
LIBERGOD <37589902+libergod@users.noreply.github.com>
2023-08-01 08:30:23 -0300

    fix: tick down prem days #1303

--- src/creatures/players/account/account.cpp
+++ src/creatures/players/account/account.cpp
@@ -405,10 +405,7 @@ error_t Account::RegisterCoinsTransaction(CoinTransactionType type, uint32_t coi
 		} else {
 			query << "`email` = " << db_->escapeString(accountIdentifier_) << " , ";
 		}
-
-		query << "`type` = " << account_type_ << " , "
-			  << "`password` = " << db_->escapeString(password_) << " , "
-			  << "`premdays` = " << premium_remaining_days_ << " , "
+			 query << "`premdays` = " << premium_remaining_days_ << " , "
 			  << "`lastday` = " << premium_last_day_;
 
 		if (id_ != 0) {
