8c99d7cd76952d935c743ce64e7917e0ac42ddc8
LIBERGOD <37589902+libergod@users.noreply.github.com>
2023-08-10 10:53:28 -0300

    improve: unnecessary files in data-canary #1320

--- data-baiak/lib/lib.lua
+++ data-baiak/lib/lib.lua
@@ -5,9 +5,6 @@ dofile(DATA_DIRECTORY.. '/lib/core/load.lua')
 -- Compatibility library for our old Lua API
 dofile(DATA_DIRECTORY.. '/lib/compat/compat.lua')
 
--- Tables library
-dofile(DATA_DIRECTORY.. '/lib/tables/load.lua')
-
 -- Others library
 dofile(DATA_DIRECTORY.. '/lib/others/load.lua')
 
