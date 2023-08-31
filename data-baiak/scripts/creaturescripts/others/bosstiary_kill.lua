e4af12e6fa8d041ac426b9355c8967e67cdfab1e
LIBERGOD <37589902+libergod@users.noreply.github.com>
2023-08-30 22:07:25 -0300

    feat: add podium of tenacity and fix bugs related to podium #1379

--- data-baiak/scripts/creaturescripts/others/bosstiary_kill.lua
+++ data-baiak/scripts/creaturescripts/others/bosstiary_kill.lua
@@ -3,15 +3,20 @@ function bosstiaryOnKill.onKill(player, creature, lastHit)
 	if not player:isPlayer() or not creature:isMonster() or creature:hasBeenSummoned() or creature:isPlayer() then
 		return true
 	end
+	local mType = MonsterType(creature:getName())
+	if not mType or not mType:bossRace() then
+		return true
+	end
 
+	local bosstiaryMultiplier = (configManager.getNumber(configKeys.BOSSTIARY_KILL_MULTIPLIER) or 1)
+	local killBonus = (configManager.getNumber(configKeys.BOOSTED_BOSS_KILL_BONUS) or 3)
 	for cid, damage in pairs(creature:getDamageMap()) do
 		local participant = Player(cid)
 		if participant and participant:isPlayer() then
 			if creature:getName():lower() == (Game.getBoostedBoss()):lower() then
-				local killBonus = configManager.getNumber(configKeys.BOOSTED_BOSS_KILL_BONUS)
-				participant:addBosstiaryKill(creature:getName(), killBonus)
+				participant:addBosstiaryKill(creature:getName(), bosstiaryMultiplier * killBonus)
 			else
-				participant:addBosstiaryKill(creature:getName())
+				participant:addBosstiaryKill(creature:getName(), bosstiaryMultiplier)
 			end
 		end
 	end
--- src/items/functions/item/item_parse.hpp
+++ src/items/functions/item/item_parse.hpp
@@ -128,6 +128,7 @@ const phmap::flat_hash_map<std::string, ItemParseAttributes_t> ItemParseAttribut
 	{ "bedpart", ITEM_PARSE_PARTNERDIRECTION },
 	{ "bedpartof", ITEM_PARSE_PARTNERDIRECTION },
 	{ "transformto", ITEM_PARSE_TRANSFORMTO },
+	{ "transformonuse", ITEM_PARSE_TRANSFORMONUSE },
 	{ "destroyto", ITEM_PARSE_DESTROYTO },
 	{ "elementice", ITEM_PARSE_ELEMENTICE },
 	{ "elementearth", ITEM_PARSE_ELEMENTEARTH },
@@ -306,6 +307,7 @@ private:
 		static void parsePerfecShot(const std::string &tmpStrValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
 		static void parseCleavePercent(const std::string &tmpStrValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
 		static void parseReflectDamage(const std::string &tmpStrValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
+		static void parseTransformOnUse(const std::string_view &tmpStrValue, pugi::xml_attribute valueAttribute, ItemType &itemType);
 
 private:
 	// Parent of the function: static void parseField
